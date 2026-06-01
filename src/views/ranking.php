9183534b662f1ec842951d12909161f8fb1d3969
Arthur Gomes <gomesarthursevero6@gmail.com>
2026-05-31 21:56:59 -0300

    Criando a page q aparecera os rankings

---
+++ src/views/ranking.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ranking - Logic Gate</title>
+    <link rel="stylesheet" href="/assets/css/style.css"> 
+</head>
+<body>
+    <header id="container-header">
+      <img src="/assets/img/logic-gate.png" alt="Logo do Logic Gate" id="logo" height="100em">
+        <ul id="list-ul">
+            <li><a href="/projeto" class="btn">Projeto</a></li>
+            <li><a href="/" class="btn">Inicio</a></li>
+            <li><a href="/login" class="btn">Entrar</a></li>
+        </ul>
+    </header>
+</body>
+</html>
