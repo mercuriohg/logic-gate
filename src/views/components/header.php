fa9ef7342cdadfe94e8f095085a32f2e1bcccea6
Arthur Gomes <gomesarthursevero6@gmail.com>
2026-09-15 00:28:24 -0300

    editando o header

--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,3 +1,11 @@
+<?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+  session_start();
+}
+
+$isAdmin = !empty($_SESSION['is_admin']);
+$username = $_SESSION['username'] ?? null;
+?>
 <header id="container-header">
   <img src="/assets/img/logic-gate.png" alt="Logo do Logic Gate" id="logo">
   <div id="toogle-div">
@@ -7,7 +15,14 @@
   <ul id="list-ul">
       <li><a href="/ranking" class="btn">Ranking</a></li>
       <li><a href="/projeto" class="btn">Projeto</a></li>
-      <li><a href="/login" class="btn">Entrar</a></li>
+      <?php if ($isAdmin): ?>
+          <li><a href="/admin" class="btn">Admin</a></li>
+          <li><a href="/logout" class="btn">Logout</a></li>
+        <?php elseif ($username): ?>
+          <li><a href="/profile" class="btn"><?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8'); ?></a></li>
+        <?php else: ?>
+          <li><a href="/login" class="btn">Login</a></li>
+      <?php endif; ?>
       <li><a href="/" class="btn">Inicio</a></li>
   </ul>
 </header>
